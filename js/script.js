cc12b4ec3294782b214f461bc7e457ac864beae3
noreen <noreennelligan1@gmail.com>
2022-12-09 19:15:08 +0000

    change color of button

--- js/script.js
+++ js/script.js
@@ -1,10 +1,28 @@
-//$('p').click(function() {
-   // $(this).slideToggle('slow');
-//})
+$(document).ready(function() {
+    $(".box").on("click", function(){
+       // var classNames = $(this).attr("class").split(" ");
+       // $("." + classNames[1]).css("background-color", "red");    
+    //} );
+//} );
 
-$('button').mouseenter(function() {
-    $(this).removeClass('makeRed').addClass('makeBlue');
+
+// * When we click on an element that has 
+ //* a 'box' class, this event will be fired.
+ 
+ var className = $(this).attr("class").split(" ");
+ var boxClass = classNames[0];
+ var className = classNames[1];
+ if ($(this).css("background-color") == "rgb(255, 0, 0)") {
+    // if this object is already red, turn it black
+    $("." + className).css("background-color", "#000");
+
+ } else {
+    //Else turn all elements with a box class black
+    //and then change the color of all elements
+    //with the same class name as the clicked element 
+    //to red.
+    $("." + boxClass).css("background-color", "#000");
+    $("." + className).css("background-color", "red");
+ }
 });
-$("button").mouseleave(function() {
-    $(this).removeClass('makeBlue').addClass('makeRed');
-});
+} )
